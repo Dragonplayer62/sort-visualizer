946206c30592e2fc5f54ac81ee49db9fec719ef7
Dragonplayer <darararagon@gmail.com>
2022-01-11 20:06:14 +0900

    Added web scraper route

--- src/$lib/nav.svelte
+++ src/$lib/nav.svelte
@@ -1,5 +1,6 @@
-<nav class="py-5 flex w-full text-gray-200">
+<nav class="py-5 flex w-full text-gray-200 bg-blue-700">
     <a class="mx-4 text-lg" href="/">Home</a>
     <a class="mx-4 text-lg" href="/sortVis">Sorting Visualizer</a>
+    <a class="mx-4 text-lg" href="/webscraper">Web scraper</a>
     <a class="mx-4 text-lg" href="/about">About</a>
 </nav>
---
+++ src/routes/webscraper.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+
+  let games = [];
+  const url = 'http://localhost:8000'
+	async function getGames(){
+		const res = await fetch(url);
+		const data = await res.json();
+		games = data;
+	}
+  onMount(()=>{
+    getGames();
+  })
+  
+</script>
+
+<main>
+  <h1 class="text-2xl text-gray-200 mx-4 my-4 text-center">Fetching 25 of this month's game releases from my web scraper microservice.</h1>
+
+  <ul class="my-4">
+    {#each games as game}
+      <li class="text-gray-200 text-center text-lg my-1">{game.name}</li>
+    {/each}
+  </ul>
+</main>
