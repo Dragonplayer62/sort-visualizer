ff8893de2e653fc280a051ca687dfbcbc8b2e62b
Dragonplayer <darararagon@gmail.com>
2022-01-25 02:27:48 +0900

    open link in new tab

--- src/routes/webscraper.svelte
+++ src/routes/webscraper.svelte
@@ -17,7 +17,7 @@
 <main class="my-4 text-2xl text-gray-200 text-center">
   <h1 class="text-4xl font-bold">Game release scraper</h1>
   <p class=" mx-4 my-4">Fetching 25 of this month's game releases from my web scraper microservice.</p>
-  <p class="text-1xl">You can find the source code <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"href="https://github.com/Dragonplayer62/web-scraper">here</a>!</p>
+  <p class="text-1xl">You can find the source code <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"href="https://github.com/Dragonplayer62/web-scraper" target="_blank" rel="noopener noreferrer">here</a>!</p>
 
   <ul class="my-4">
     {#each games as game}
